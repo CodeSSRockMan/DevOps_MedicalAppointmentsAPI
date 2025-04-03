8e5f43945cab0b48f081275bea61c063c5483311
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-03 20:44:18 -0300

    preparing merge

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///schedule.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Convierte a booleano
+    DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Converts to boolean value
     
