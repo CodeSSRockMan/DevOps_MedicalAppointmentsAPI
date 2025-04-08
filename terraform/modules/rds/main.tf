b1505fd4df03defc1d8107f01f0e483786918bde
Mathias Andreas Von Jentschyk Makuc <mathiasvon7@gmail.com>
2025-04-08 13:55:34 -0400

    Eliminated the egress rule for RDS because the database doesn't need outbound connections.

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -18,14 +18,9 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [var.ec2_sg_id]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
+# Eliminated the egress rule for RDS because the database doesn't need outbound connections.
 
 resource "aws_db_instance" "default" {
   allocated_storage      = 20
